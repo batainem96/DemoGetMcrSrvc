75c90d841404411633f5494dec79b9f4fd556adb
batainem96 <batainem96@gmail.com>
2022-04-05 19:44:53 -0500

    Initialized RecordsService class and respective JUnit test class.

---
+++ src/main/java/com/demo/mcrsrvc/services/RecordsService.java
@@ -0,0 +1,11 @@
+package com.demo.mcrsrvc.services;
+
+/**
+ * The RecordsService Class is responsible for applying business logic to incoming HTTP Requests from the Controller
+ * layer and passing them to the Data Access layer for querying the Employee Records collection.
+ */
+public class RecordsService {
+
+
+
+}
---
+++ src/test/java/com/demo/mcrsrvc/services/RecordsServiceTests.java
@@ -0,0 +1,10 @@
+package com.demo.mcrsrvc.services;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RecordsServiceTests {
+
+
+
+}
