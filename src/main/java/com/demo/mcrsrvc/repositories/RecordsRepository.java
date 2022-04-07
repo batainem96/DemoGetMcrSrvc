8fabb2704553d1873082798e4d8050680e0b84b0
batainem96 <batainem96@gmail.com>
2022-04-07 17:26:24 -0500

    Cleaned up RecordsRepository (unused methods, keeping it simple).

--- src/main/java/com/demo/mcrsrvc/repositories/RecordsRepository.java
+++ src/main/java/com/demo/mcrsrvc/repositories/RecordsRepository.java
@@ -5,18 +5,11 @@ import org.springframework.data.mongodb.repository.Query;
 import com.demo.mcrsrvc.models.EmployeeRecord;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 @Component
 public interface RecordsRepository extends MongoRepository<EmployeeRecord, String> {
 
     @Query("{empId:'?0'}")
     EmployeeRecord findRecordByEmpId(String empId);
-
-    @Query(value="{category:'?0'}", fields="{'firstName' : 1, 'quantity' : 1}")
-    List<EmployeeRecord> findAll(String category);
-
-    public long count();
 
 }
 
