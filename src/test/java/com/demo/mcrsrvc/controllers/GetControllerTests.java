53b02d3b1da1d8ec12bf957cedea6392ef30a245
batainem96 <batainem96@gmail.com>
2022-04-07 17:09:43 -0500

    Begun GetController testing. Fixed issue in which RecordsServiceTests was instantiating live RecordsRepository object (only mocked instance was used), and cleaned up warnings.

---
+++ src/test/java/com/demo/mcrsrvc/controllers/GetControllerTests.java
@@ -0,0 +1,10 @@
+package com.demo.mcrsrvc.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GetControllerTests {
+
+    GetController getController;
+
+}
--- src/test/java/com/demo/mcrsrvc/services/RecordsServiceTests.java
+++ src/test/java/com/demo/mcrsrvc/services/RecordsServiceTests.java
@@ -1,8 +1,9 @@
 package com.demo.mcrsrvc.services;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.demo.mcrsrvc.models.EmployeeRecord;
 import com.demo.mcrsrvc.repositories.RecordsRepository;
 import org.mockito.Mockito;
@@ -14,23 +15,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class RecordsServiceTests {
 
+    @InjectMocks
     private RecordsService recordsService;
 
-    @Autowired
-    public RecordsServiceTests(RecordsService recordsService) {
-        this.recordsService = recordsService;
-    }
+    @Mock
+    RecordsRepository recordsRepository;
 
     @Test
     public void whenValidEmpIdGiven_andEmpIdMatchFound_thenAssertionSucceeds() {
 
         EmployeeRecord expectedRecord = new EmployeeRecord("123", "John", "Doe", "IT");
         String testEmpId = "123";
-        RecordsRepository mockRecordsRepository = Mockito.mock(RecordsRepository.class);
 
-        Mockito.when(mockRecordsRepository.findRecordByEmpId(testEmpId)).thenReturn(expectedRecord);
+        Mockito.when(recordsRepository.findRecordByEmpId(testEmpId)).thenReturn(expectedRecord);
 
-        EmployeeRecord actualRecord = mockRecordsRepository.findRecordByEmpId(testEmpId);
+        EmployeeRecord actualRecord = recordsRepository.findRecordByEmpId(testEmpId);
 
         assertEquals(actualRecord, expectedRecord);
 
@@ -39,15 +38,13 @@ public class RecordsServiceTests {
     @Test
     public void whenValidEmpIdGiven_andEmpIdMatchNotFound_andResourceNotFoundExceptionThrown_thenAssertionSucceeds() {
 
-        EmployeeRecord expectedRecord = new EmployeeRecord("123", "John", "Doe", "IT");
         String testEmpId = "444";
-        RecordsRepository mockRecordsRepository = Mockito.mock(RecordsRepository.class);
 
-        Mockito.when(mockRecordsRepository.findRecordByEmpId(testEmpId)).thenReturn(null);
+        Mockito.when(recordsRepository.findRecordByEmpId(testEmpId)).thenReturn(null);
 
-        Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-            recordsService.getRecordByEmpId(testEmpId);
-        }); // Non-matching empId returns null from RecordsRepository and throws ResourceNotFoundException
+        Exception exception = assertThrows(ResourceNotFoundException.class, () ->
+                recordsService.getRecordByEmpId(testEmpId)
+        ); // Non-matching empId returns null from RecordsRepository and throws ResourceNotFoundException
 
         // Exception message should match String below
         String expectedMessage = "No record found under empId: " + testEmpId;
@@ -60,17 +57,17 @@ public class RecordsServiceTests {
     @Test
     public void whenInvalidEmpIdGiven_andInvalidRequestExceptionThrown_thenAssertionSucceeds() {
 
-        Exception exception_1 = assertThrows(InvalidRequestException.class, () -> {
-            recordsService.getRecordByEmpId("");
-        }); // Empty empId throws InvalidRequestException
+        Exception exception_1 = assertThrows(InvalidRequestException.class, () ->
+                recordsService.getRecordByEmpId("")
+        ); // Empty empId throws InvalidRequestException
 
-        Exception exception_2 = assertThrows(InvalidRequestException.class, () -> {
-            recordsService.getRecordByEmpId("  ");
-        }); // Empty empId w/ spaces throws InvalidRequestException
+        Exception exception_2 = assertThrows(InvalidRequestException.class, () ->
+                recordsService.getRecordByEmpId("  ")
+        ); // Empty empId w/ spaces throws InvalidRequestException
 
-        Exception exception_3 = assertThrows(InvalidRequestException.class, () -> {
-            recordsService.getRecordByEmpId(null);
-        }); // Null empId throws InvalidRequestException
+        Exception exception_3 = assertThrows(InvalidRequestException.class, () ->
+                recordsService.getRecordByEmpId(null)
+        ); // Null empId throws InvalidRequestException
 
         // Exception message should match String below
         String expectedMessage = "empId is invalid!";
